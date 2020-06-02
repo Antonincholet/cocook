dda141148f148d14f221511c5f9aebcd20e921ae
olivier Tengherian <oliver.hyperion@gmx.fr>
2020-06-02 15:39:10 +0200

    Add Controler Offers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/offers', to: 'offers#index'
+  # get '/offers/:id', to: 'offers#show', as: 'offer'
+  # get '/offers/new', to: 'offers#new'
+  # post '/offers', to: 'offers#create'
+  # update
+  # destroy
+
+
+  resources :offers, except: [ :edit, :update ]
+
+
+
+
 end
