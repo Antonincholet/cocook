d47c37383dc571d3717ac7b5ec46421ee13d8f43
antonin cholet <antonin@cholet-avocat.fr>
2020-06-02 17:06:11 +0200

    add method dashboard to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @user = current_user
+    @offers = @user.offers
+    @reservations = @user.reservations
+  end
 end
