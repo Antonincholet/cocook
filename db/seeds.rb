5a23517bf0d9a66b8ad1adaede478656ff73f3f8
Antonin Cholet <46227773+Antonincholet@users.noreply.github.com>
2020-06-03 10:45:06 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ require 'faker'
 CUISINES = ['french', 'asian', 'japanese', 'italian', 'oriental', 'greek', 'spanish', 'swedish', 'fast food', 'argentine', 'brazilian', 'street food']
 I18n.locale = 'fr-FR'
 
-time.20 do |x|
+time.2 do |x|
   user = User.new
   user.email = Faker::Internet.free_email
   user.password = 'password'
@@ -32,13 +32,13 @@ end
 
 time.10 do |x|
   offer = Offer.new
-  offer.label = Faker::Internet.email
+  offer.label = Faker::Food.dish
   offer.description = "#{offer.label} comme au restaurant à votre domicile"
   offer.cuisine = CUISINES.sample
   offer.address = Faker::Address.full_address
   offer.km_perimeter = rand(1..50)
   offer.rate_per_guest = rand(15..35)
-  offer.user_id = rand(1..20)
+  offer.user_id = rand(1..2)
   if offer.save
     puts "offer #{x} created"
   else
@@ -46,12 +46,12 @@ time.10 do |x|
   end
 end
 
-time.2 do |x|
+time.10 do |x|
   reservation = Reservation.new
   reservation.address = Faker::Address.full_address
   reservation.date_time = Faker::Time.between(from: DateTime.now - 365, to: DateTime.now, format: :default)
   reservation.guests_count = rand(1..8)
-  resa.user_id = rand(1..20)
+  resa.user_id = rand(1..2)
   resa.offer_id = rand(1..10)
   if reservation.save
     puts "reservation #{x} created"
